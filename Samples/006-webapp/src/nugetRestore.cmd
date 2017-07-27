67b5d307adbd074c098c00fdfb462dd015a0e8d6
Andrew Ivanov <anivan@microsoft.com>
2017-07-27 20:41:40 +0000

    fixing nuget path in the webapp

--- Samples/006-webapp/src/nugetRestore.cmd
+++ Samples/006-webapp/src/nugetRestore.cmd
@@ -1,1 +1,9 @@
-nuget restore -SolutionDirectory ./
+@echo off
+set NUGET_COMMAND=nuget                          
+set APPSERVICE_NUGET_PATH=D:\Program Files (x86)\SiteExtensions\Kudu\63.60712.2926\bin\Scripts\nuget.exe
+
+IF EXIST %APPSERVICE_NUGET_PATH% (
+	set NUGET_COMMAND="%APPSERVICE_NUGET_PATH%"
+)
+@echo on
+%NUGET_COMMAND% restore -SolutionDirectory ./
