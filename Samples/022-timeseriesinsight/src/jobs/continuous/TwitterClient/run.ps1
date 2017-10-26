a487be6fa55163df0a6992b0643d0683841d7789
DataSnowman <darsch@microsoft.com>
2017-10-26 14:47:22 -0700

    delete dup

--- Samples/022-timeseriesinsight/src/jobs/continuous/TwitterClient/run.ps1
+++
@@ -1,4 +0,0 @@
-$sitePackagesDirectory = 'site-packages'
-py -m pip install -t "$sitePackagesDirectory" -r requirements.txt
-$env:PYTHONPATH = $(Resolve-Path -Path "$sitePackagesDirectory").Path
-py twitterClient.py
